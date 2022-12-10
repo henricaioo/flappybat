c9f183249bcbc290e8e4b0b7f98b3ed18fdd9c3f
henricaioo <101869034+henricaioo@users.noreply.github.com>
2022-12-10 17:14:25 -0300

    Update playnow.php

--- playnow.php
+++ playnow.php
@@ -1,3 +1,3 @@
 <div class="my-5">
-    <a class="play d-inline d-lg-none" href="./adm/login.php">Jogue agora!!!</a>
+    <a class="play d-inline d-lg-none" href="login.php">Jogue agora!!!</a>
 </div>
