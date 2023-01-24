b04593459b98c2eb81fb4ee261286fb96283d075
henricaioo <101869034+henricaioo@users.noreply.github.com>
2023-01-23 22:11:53 -0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php 
-    $hostname = 'sql102.epizy.com';
-    $user = 'epiz_31716378';
-    $password = 'K6pA7zgvngEe1';
+    $hostname = '191.252.101.191';
+    $user = 'caioh';
+    $password = 'caioh@21012023';
     $database = 'epiz_31716378_flappybat';
  
     $connect = mysqli_connect($hostname, $user, $password ,$database) or die ("Não foi possível realizar a conexão");
