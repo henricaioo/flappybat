a1003142d1b52ed07cf246cf70cf10f0b5cc21f7
henricaioo <101869034+henricaioo@users.noreply.github.com>
2023-01-23 22:34:45 -0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,21 +1,41 @@
-<?php
-    ini_set('error_reporting', E_ALL);
-    ini_set('display_errors', 1);
-    require("adm/connect.php");
-
-    $username = (isset($_POST['username'])) ? $_POST['username'] : "";
-    $email = (isset($_POST['email'])) ? $_POST['email'] : "";
-    $pais = (isset($_POST['pais'])) ? $_POST['pais'] : "";
-    $senha = (isset($_POST['senha'])) ? $_POST['senha'] : "";
-
-    $sql_insert = "INSERT INTO usuario (username, email, pais, senha, ranking, horas_jogadas, partidas_jogadas, pontuacao_max, moedas, skin_utilizada, icon) 
-    VALUES ('$username', '$email' , '$pais' , '$senha', '-', '0', 0, 0, 0, 1, 1)";
-
-    if(mysqli_query($connect,$sql_insert)){
-        header("location: index.php");
-    }else{
-        print 'Não foi possível incluir os dados!';
-    }
-            
-    mysqli_close($connect);
+<?php
+
+    ini_set('error_reporting', E_ALL);
+
+    ini_set('display_errors', 1);
+
+    require("adm/connect.php");
+
+
+
+    $username = (isset($_POST['username'])) ? $_POST['username'] : "";
+
+    $email = (isset($_POST['email'])) ? $_POST['email'] : "";
+
+    $pais = (isset($_POST['pais'])) ? $_POST['pais'] : "";
+
+    $senha = (isset($_POST['senha'])) ? $_POST['senha'] : "";
+
+
+
+    $sql_insert = "INSERT INTO usuario (username, email, pais, senha) 
+
+    VALUES ('$username', '$email' , '$pais' , '$senha')";
+
+
+
+    if(mysqli_query($connect,$sql_insert)){
+
+        header("location: index.php");
+
+    }else{
+
+        print 'Não foi possível incluir os dados!';
+
+    }
+
+            
+
+    mysqli_close($connect);
+
 ?>
