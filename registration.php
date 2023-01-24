113373fc1a921ced5b32f8f739426f8d69a50d6b
henricaioo <12201000103@muz.ifsuldeminas.edu.br>
2023-01-23 23:07:40 -0300

    nome

--- registration.php
+++ registration.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
     ini_set('error_reporting', E_ALL);
@@ -39,46 +38,3 @@
     mysqli_close($connect);
 
 ?>
-=======
-<?php
-
-    ini_set('error_reporting', E_ALL);
-
-    ini_set('display_errors', 1);
-
-    require("adm/connect.php");
-
-
-
-    $username = (isset($_POST['username'])) ? $_POST['username'] : "";
-
-    $email = (isset($_POST['email'])) ? $_POST['email'] : "";
-
-    $pais = (isset($_POST['pais'])) ? $_POST['pais'] : "";
-
-    $senha = (isset($_POST['senha'])) ? $_POST['senha'] : "";
-
-
-
-    $sql_insert = "INSERT INTO usuario (username, email, pais, senha) 
-
-    VALUES ('$username', '$email' , '$pais' , '$senha')";
-
-
-
-    if(mysqli_query($connect,$sql_insert)){
-
-        header("location: index.php");
-
-    }else{
-
-        print 'Não foi possível incluir os dados!';
-
-    }
-
-            
-
-    mysqli_close($connect);
-
-?>
->>>>>>> a1003142d1b52ed07cf246cf70cf10f0b5cc21f7
