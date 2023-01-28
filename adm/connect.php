8394dfb86fedb723f61ab6a9417f55f97cd1c96c
henricaioo <101869034+henricaioo@users.noreply.github.com>
2023-01-27 21:09:04 -0300

    Update connect.php

--- adm/connect.php
+++ adm/connect.php
@@ -1,7 +1,7 @@
 <?php 
-    $hostname = 'localhost';
-    $user = 'root';
-    $password = '';
+    $hostname = '191.252.101.191';
+    $user = 'caioh';
+    $password = 'caioh@21012023';
     $database = 'epiz_31716378_flappybat';
  
     $connect = mysqli_connect($hostname, $user, $password ,$database) or die ("Não foi possível realizar a conexão");
